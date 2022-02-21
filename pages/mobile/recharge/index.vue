d7dddfcd8ccf607a1de98db1b478f6a71cb933e3
shdboom <928421021@qq.com>
2022-02-21 14:27:09 +0800

    手机充值页面开发

--- pages/mobile/recharge/index.vue
+++ pages/mobile/recharge/index.vue
@@ -12,33 +12,34 @@
         <div class='mg-tp-20'>
           <div class="main-content">
             <el-form ref="form" :model="form" label-width="80px">
-              <el-form-item>
-                <div class="recharge-company">
-                  <el-radio-group v-model="form.company">
-                    <el-radio :label="1" border>
-                      <img :src="sinopecUrl">
-                      <img :class="form.company==1?'active':''" class="pick-img" :src="pickUrl">
-                    </el-radio>
-                    <el-radio :label="2" border>
-                      <img :src="petroChinaUrl">
-                      <img :class="form.company==2?'active':''" class="pick-img" :src="pickUrl">
-                    </el-radio>
-                  </el-radio-group>
-                </div>
+              <el-form-item label="手机号码">
+                <el-input class="recharge-input" v-model="form.card" placeholder="请输入手机号码"></el-input>
               </el-form-item>
-              <el-form-item label="加油卡号">
-                <el-input class="recharge-input" v-model="form.card" placeholder="请输入16位石油加油卡卡号"></el-input>
+              <el-form-item label="运营商">
+                <span>中国移动</span>
               </el-form-item>
               <el-form-item label="充值金额">
                 <el-radio-group v-model="form.price">
-                  <el-radio :label="200" border>
-                  </el-radio>
-                  <el-radio :label="300" border>
-                  </el-radio>
-                  <el-radio :label="500" border>
-                  </el-radio>
-                  <el-radio :label="1000" border>
-                  </el-radio>
+                  <div class="recharge-radio flex-r">
+                    <el-radio :label="20" border>
+                      20元
+                    </el-radio>
+                    <el-radio :label="30" border>
+                      30元
+                    </el-radio>
+                    <el-radio :label="50" border>
+                      50元
+                    </el-radio>
+                    <el-radio :label="100" border>
+                      100元
+                    </el-radio>
+                    <el-radio :label="200" border>
+                      200元
+                    </el-radio>
+                    <el-radio :label="300" border>
+                      300元
+                    </el-radio>
+                  </div>
                 </el-radio-group>
               </el-form-item>
               <el-form-item label="接口价格">
